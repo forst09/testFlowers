63af330a0abd456a71debd22f8529abbe63125b3
forst09 <dforst09@gmail.com>
2022-05-13 02:16:47 +0300

    add pop-up

---
+++ script.js
@@ -0,0 +1,58 @@
+function noDigits(event) {
+    if ("1234567890".indexOf(event.key) != -1)
+        event.preventDefault();
+}
+function onlyDigits(event) {
+    if ("1234567890+".indexOf(event.key) == -1)
+        event.preventDefault();
+}
+function validateForm() {
+    var a = document.forms["Form"]["userName"].value;
+    var b = document.forms["Form"]["userPhone"].value;
+    if (a == null || a == "" || b == null || b == "") {
+        alert("Необходимо заполнить все поля ввода");
+        return false;
+    }
+}
+
+
+
+
+const popupLinks = document.querySelectorAll('.popup-link');
+
+if (popupLinks.length > 0) {
+    for (let index = 0; index < popupLinks.length; index++) {
+        const popupLink = popupLinks[index];
+        popupLink.addEventListener("click", function (e) {
+            const popupName = popupLink.getAttribute('href').replace('#', '');
+            const curentPopup = document.getElementById(popupName);
+            popupOpen(curentPopup);
+            e.preventDefault();
+        });
+    }
+}
+
+
+const popupCloseIcon = document.querySelectorAll('.popup-close');
+if (popupCloseIcon.length > 0) {
+    for (let index = 0; index < popupCloseIcon.length; index++) {
+        const el = popupCloseIcon[index];
+        el.addEventListener('click', function (e) {
+            popupClose(el.closest('.popup'));
+            e.preventDefault();
+        });
+    }
+}
+
+function popupOpen(curentPopup) {
+    curentPopup.classList.add('open');
+    curentPopup.addEventListener("click", function (e) {
+        if (!e.target.closest('.popup__content')) {
+            popupClose(e.target.closest('.popup'));
+        }
+    });
+}
+
+function popupClose(curentPopup) {
+    curentPopup.classList.remove('open');
+}
